e9e402cbb0bca312e1880b56e1d3b0d6e8b44095
Vinicius Doneli <vinidoneli@gmail.com>
2026-06-25 22:19:44 -0300

    Att Server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors    = require('cors');
+const path    = require('path');
 
 const NotificacaoController = require('./src/js/controller/NotificacaoController');
 const PostController        = require('./src/js/controller/PostController');
@@ -23,7 +24,13 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-app.use(express.static('public'));
+// Garante que o Express ache a pasta public, independente de onde a Vercel rode o código
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota explícita para a página inicial
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // USUÁRIOS
 app.post('/usuarios/cadastro',      (req, res) => UsuarioController.cadastrar(req, res));
